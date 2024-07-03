635c0790b03e400865bd0cd460d57f85859fc4ac
Your Name <your.email@example.com>
2024-07-03 15:32:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+
+<template>
+<!-- <div id = "app">
+    <div class="container">
+        <div class="main">
+            <div class="header">
+                <h2>End Band</h2>
+            </div>
+
+            
+              <div class="song-img"></div>
+            <div class="song-detail">
+              <h2>One Last Kiss</h2>
+              <h3>宇</h3>
+            </div>
+
+            <div class="song-controls">
+
+              <button class="prev">
+                <span>
+                  <i class="bx bx-skip-previous bx-md"></i>
+                </span>
+              </button>
+
+              <button class="play">
+                <span>
+                  <i class="bx bx-play bx-md"></i>
+                </span>
+              </button>
+
+              <button class="next">
+                <span>
+                  <i class="bx bx-skip-next bx-md"></i>
+                </span>
+              </button>
+
+
+            </div>
+        </div>
+    </div>
+</div> -->
+
+
+</template>
+
+
+
+<script>
+
+</script>
